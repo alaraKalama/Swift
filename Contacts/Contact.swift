826579c4b39145c135695e6b2abde6943174a635
Bianca Hinova <bianca.hinova@gmail.com>
2016-06-20 00:12:47 +0300

    contacts app - entity created

---
+++ Contacts/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Bianca Hinova on 6/19/16.
+//  Copyright © 2016 bianca. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
